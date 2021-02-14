f401c613f9b5e0b30193f97bcd924087ab47976d
netdevaustralia <vmahawar@live.com>
2021-02-14 18:34:12 +1100

    Develop (#2)
    * WIP :: Folder structure changes
    
    * WIP:: Added test project and business logic implementation
    
    * WIP:: folder structure changed
    
    * WIP:: Folder structure changes
    
    * Updated readme.
    
    * Update README.md
    
    * Updated read me
    
    * removed todo
    
    * Code cleanup
    
    * project architecture diagram
    
    * added description
    
    * image
    
    * removed unused link
    
    * fixed logging error
    
    * Added docker and enabled CORS
    
    * Update Dockerfile
    
    * Adding workflow file
    
    * delete workflow
    
    * removed docker and setup swagger url

--- src/ShoppingCart/Startup.cs
+++ src/ShoppingCart/Startup.cs
@@ -1,6 +1,5 @@
 namespace ShoppingCart.Api
 {
-    using System.Text.Json.Serialization;
     using Application;
     using Application.Config;
     using Infrastructure;
@@ -10,6 +9,7 @@ namespace ShoppingCart.Api
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
     using Microsoft.OpenApi.Models;
+    using System.Text.Json.Serialization;
 
     public class Startup
     {
@@ -33,7 +33,7 @@ namespace ShoppingCart.Api
                     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "ShoppingCart", Version = "v1"});
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ShoppingCart", Version = "v1" });
             });
         }
 
@@ -41,13 +41,19 @@ namespace ShoppingCart.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ShoppingCart v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ShoppingCart v1");
+                    c.RoutePrefix = string.Empty;
+                });
 
             app.UseHttpsRedirection();
 
